44d6f45e4db5562e70d72a80aa816770187bdbaa
mmcglynn45 <ndmmcglynn@gmail.com>
2015-10-03 21:42:21 -0700

    Build Update

---
+++ Script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  Script.sh
+#  PIFlightController
+#
+#  Created by Matthew McGlynn on 10/3/15.
+#  Copyright (c) 2015 Matthew McGlynn. All rights reserved.
+
+cd /Users/mmcglynn/Desktop/PIFlightController
+git commit -m "Build Update" -a
+git push
