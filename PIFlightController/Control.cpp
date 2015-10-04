7a008824ebf9d13cf4aa203ee44b6a6179b8f2eb
mmcglynn45 <ndmmcglynn@gmail.com>
2015-10-03 23:39:50 -0700

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -20,54 +20,54 @@
 Control::Control(){}
     
     
-    void Control::setup(){
-        int fd = pca9685Setup(PIN_BASE, 0x40, HERTZ);
-        if (fd < 0)
-        {
-            printf("Error in setup\n");
-        }
-        
-        // Reset all output
-        pca9685PWMReset(fd);
-    }
-    
-    void Control::demo(){
-        // Set servo to neutral position at 1.5 milliseconds
-        // (View http://en.wikipedia.org/wiki/Servo_control#Pulse_duration)
-        float millis = 1.0;
-        int tick = calcTicks(millis, HERTZ);
-        while(1){
-            pwmWrite(PIN_BASE + 16, tick);
-            delay(2000);
-            millis=1.6;
-            tick = calcTicks(millis, HERTZ);
-            pwmWrite(PIN_BASE + 16, tick);
-            delay(2000);
-            millis=1.0;
-            tick = calcTicks(millis, HERTZ);
-            pwmWrite(PIN_BASE + 16, tick);
-            delay(2000);
-        }
-    }
-    
-    
-    /**
-     * Calculate the number of ticks the signal should be high for the required amount of time
-     */
-    int Control::calcTicks(float impulseMs, int hertz)
+void Control::setup(){
+    int fd = pca9685Setup(PIN_BASE, 0x40, HERTZ);
+    if (fd < 0)
     {
-        float cycleMs = 1000.0f / hertz;
-        return (int)(MAX_PWM * impulseMs / cycleMs + 0.5f);
+        printf("Error in setup\n");
     }
     
-    /**
-     * input is [0..1]
-     * output is [min..max]
-     */
-    float Control::map(float input, float min, float max)
-    {
-        return (input * max) + (1 - input) * min;
+    // Reset all output
+    pca9685PWMReset(fd);
+}
+
+void Control::demo(){
+    // Set servo to neutral position at 1.5 milliseconds
+    // (View http://en.wikipedia.org/wiki/Servo_control#Pulse_duration)
+    float millis = 1.0;
+    int tick = calcTicks(millis, HERTZ);
+    while(1){
+        pwmWrite(PIN_BASE + 16, tick);
+        delay(2000);
+        millis=1.6;
+        tick = calcTicks(millis, HERTZ);
+        pwmWrite(PIN_BASE + 16, tick);
+        delay(2000);
+        millis=1.0;
+        tick = calcTicks(millis, HERTZ);
+        pwmWrite(PIN_BASE + 16, tick);
+        delay(2000);
     }
+}
+
+
+/**
+ * Calculate the number of ticks the signal should be high for the required amount of time
+ */
+int Control::calcTicks(float impulseMs, int hertz)
+{
+    float cycleMs = 1000.0f / hertz;
+    return (int)(MAX_PWM * impulseMs / cycleMs + 0.5f);
+}
+
+/**
+ * input is [0..1]
+ * output is [min..max]
+ */
+float Control::map(float input, float min, float max)
+{
+    return (input * max) + (1 - input) * min;
+}
 
 
 
