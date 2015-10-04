9da740bea4ca58ed1f223728d8535dc58a954bd2
mmcglynn45 <ndmmcglynn@gmail.com>
2015-10-03 21:38:05 -0700

    Build Update

--- PIFlightController/main.cpp
+++ PIFlightController/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "How did the Irish lose! Twice??? I could do better.";
+    std::cout << "How did the Irish lose! Twice??? I could do better. Twice as well.";
     
     return 0;
 }
