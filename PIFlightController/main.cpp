64994ed647f4330c1692f22cefe79769f05f9409
mmcglynn45 <ndmmcglynn@gmail.com>
2015-10-08 00:42:57 -0700

    Build Update

--- PIFlightController/main.cpp
+++ PIFlightController/main.cpp
@@ -32,13 +32,17 @@ int main(void)
     
     IMU piIMU;
     piIMU.setup();
-    pthread_t thread1,thread2,thread3,thread4;
+    pthread_t thread1,thread2,thread3,thread4,thread5,thread6,thread7,thread8;
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i<5000; i++) {
         pthread_create(&thread1, NULL, threaded, &controller);
         pthread_create(&thread2, NULL, threaded, &controller);
         pthread_create(&thread3, NULL, threaded, &controller);
         pthread_create(&thread4, NULL, threaded, &controller);
+        pthread_create(&thread5, NULL, threaded, &controller);
+        pthread_create(&thread6, NULL, threaded, &controller);
+        pthread_create(&thread7, NULL, threaded, &controller);
+        pthread_create(&thread8, NULL, threaded, &controller);
         while(!piIMU.updateIMU()){
             //i--;
         }
@@ -46,6 +50,10 @@ int main(void)
         pthread_join( thread2, NULL);
         pthread_join( thread3, NULL);
         pthread_join( thread4, NULL);
+        pthread_join( thread5, NULL);
+        pthread_join( thread6, NULL);
+        pthread_join( thread7, NULL);
+        pthread_join( thread8, NULL);
         //controller.adjustYPMotor(0.2);
         //controller.adjustXPMotor(0.7);
         //controller.adjustYNMotor(0.7);
@@ -78,7 +86,7 @@ void* threaded(void * controller){
     Control * controller2;
     controller2 = (Control *)controller;
     controller2->adjustYPMotor(0.2);
-    controller2->adjustYNMotor(0.2);
+    //controller2->adjustYNMotor(0.2);
     //printf("Hi");
     return NULL;
 }
