41d857832fdbf47c728366579812c1ed1c63f927
mmcglynn45 <ndmmcglynn@gmail.com>
2015-11-01 01:32:18 -0800

    Build Update

--- PIFlightController/IMU.cpp
+++ PIFlightController/IMU.cpp
@@ -38,7 +38,7 @@ int IMU::updateIMU(){
         //roll = to_degrees(imuData.fusionPose.x());
         //pitch = to_degrees(imuData.fusionPose.y());
         //yaw = to_degrees(imuData.fusionPose.z());
-        rotation(0,imuData.fusionPose.x(),imuData.fusionPose.y(),imuData.fusionPose.z());
+        rotation(1,imuData.fusionPose.x(),imuData.fusionPose.y(),imuData.fusionPose.z());
         //printf("Test one piece: Roll = %f\n",to_degrees(imuData.fusionPose.data(0)));
         //printf("Sample rate %d: %s\r", sampleRate, RTMath::displayDegrees("", imuData.fusionPose));
         return 1;
