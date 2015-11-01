4159e22c391531e0c3cacf3e89cb66f74b58c2e1
mmcglynn45 <ndmmcglynn@gmail.com>
2015-11-01 01:15:09 -0700

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -77,7 +77,7 @@ void Control::ManageOrientation(double roll, double pitch, double yaw){
 
 double Control::PitchPIDComputation(double current, double desired){
     double Kp = 0.01;
-    double Ki = 0.001;
+    double Ki = 0.0001;
     double Kd = 0.005;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-pitchTime);
