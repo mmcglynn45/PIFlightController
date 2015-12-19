4d33cd9f36cf1ead990397e1621e050189221f1d
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-19 14:42:15 -0800

    Build Update

--- PIFlightController/Sonar.cpp
+++ PIFlightController/Sonar.cpp
@@ -30,7 +30,7 @@ double Sonar::getCM() {
     
     //Wait for echo start
     while(digitalRead(ECHO) == LOW){
-        if ((micros()-startTime)>10000) {
+        if ((micros()-startTime)>100) {
             return distance;
         }
     }
@@ -38,7 +38,7 @@ double Sonar::getCM() {
     //Wait for echo end
     startTime = micros();
     while(digitalRead(ECHO) == HIGH){
-        if ((micros()-startTime)>10000) {
+        if ((micros()-startTime)>100) {
             return distance;
         }
     }
