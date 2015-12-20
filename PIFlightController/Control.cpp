2828696c9500219cdb21d219e26b852a69d19c63
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-19 23:40:44 -0800

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -253,8 +253,8 @@ void Control::MapMotorOutput(double pitchControl,double rollControl, double yawC
     double yawN = 1 - yawP;
     double XPSpeed = throttleBaseline * pitchP * rollP *yawP;
     double XNSpeed = throttleBaseline * pitchP * rollN *yawN;
-    double YPSpeed = throttleBaseline * pitchN * rollP *yawN;
-    double YNSpeed = throttleBaseline * pitchN * rollN *yawP;
+    double YPSpeed = throttleBaseline * pitchN * rollN *yawN;
+    double YNSpeed = throttleBaseline * pitchN * rollP *yawP;
     XPSpeed = inputNormalizer(XPSpeed, 0, 1);
     XNSpeed = inputNormalizer(XNSpeed, 0, 1);
     YPSpeed = inputNormalizer(YPSpeed, 0, 1);
