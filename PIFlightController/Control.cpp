47bcac7fa707888dd6cdce820e0b9e1bd9db44f8
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-20 15:55:48 -0800

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -139,7 +139,7 @@ double Control::mXPIDComputation(double current, double desired){
     printf("current: %f \n", current);
     printf("desired: %f \n", desired);
     double Kp = 0.3;
-    double Ki = 1.2;
+    double Ki = 2.2;
     double Kd = 0.02;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-mxTime);
@@ -158,7 +158,7 @@ double Control::mYPIDComputation(double current, double desired){
     printf("current: %f \n", current);
     printf("desired: %f \n", desired);
     double Kp = 0.3;
-    double Ki = 1.2;
+    double Ki = 2.2;
     double Kd = 0.02;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-myTime);
