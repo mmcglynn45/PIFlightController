d7b41a01bcf533e4c017431a52802d6bf2417ffc
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-19 17:30:37 -0800

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -126,7 +126,7 @@ double Control::AltitudePIDComputation(double current, double desired){
     printf("desired: %f \n", desired);
     double Kp = 0.1;
     double Ki = 0.05;
-    double Kd = 0.001;
+    double Kd = 0.005;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-altitudeTime);
     double deltaT = time_span.count();
