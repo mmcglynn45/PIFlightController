3071c1d9b0479e3ca6350559dae9c2ed1b22ae9b
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-20 16:38:01 -0800

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -120,7 +120,7 @@ void Control::adjustMotorSpeed(int motor, double speed){
 void Control::ManageOrientation(double roll, double pitch, double yaw, double altitude, double mX, double mY){
     double desiredPitch = mXPIDComputation(mX, 0);
     double desiredRoll = mYPIDComputation(mY, 0);
-    desiredPitch = inputNormalizer(desiredPitch, -2, 2);
+    desiredPitch = inputNormalizer(-desiredPitch, -2, 2);
     desiredRoll = inputNormalizer(desiredRoll, -2, 2);
     printf("desiredRoll: %f \n", desiredRoll);
     printf("desiredPitch: %f \n", desiredPitch);
