b0e176b95ba4fa8189f6517cb330035a8c9c25a0
mmcglynn45 <ndmmcglynn@gmail.com>
2015-12-23 00:51:02 -0800

    Build Update

--- PIFlightController/main.cpp
+++ PIFlightController/main.cpp
@@ -98,7 +98,7 @@ int main(void)
         //cout << "Total mX drift (meters) = " << xPosDrift << endl;
         //cout << "Total mY drift (meters) = " << yPosDrift << endl;
         //cout << "Total Distance (meters) = " << sqrt(xPosDrift*xPosDrift + yPosDrift*yPosDrift) << endl;
-        cout<< "Throttle equals " << radio.throttle << endl;
+        //cout<< "Throttle equals " << radio.throttle << endl;
         
         //printf("Sonar Reading: %f \n",firstSonar.distance);
         if (!controller.safetyCheck(piIMU.roll, piIMU.pitch)) {
