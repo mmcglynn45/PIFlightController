758d98c32858e084bc4a025099a4054202f76844
mmcglynn45 <ndmmcglynn@gmail.com>
2016-02-06 03:11:54 -0800

    Build Update

--- PIFlightController/IMU.cpp
+++ PIFlightController/IMU.cpp
@@ -32,7 +32,6 @@ int IMU::updateIMU(){
     if (imu->IMURead()) {
         RTIMU_DATA imuData = imu->getIMUData();
         while (imu->IMURead()) {
-            printf("Stuck in loop\n");
             imuData = imu->getIMUData();
         }
         sampleCount++;
