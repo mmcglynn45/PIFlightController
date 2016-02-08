d37f9e95b9d3d925989b0c1b9a4ba1e7c76ceca5
mmcglynn45 <ndmmcglynn@gmail.com>
2016-02-07 23:05:49 -0800

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -248,8 +248,8 @@ double Control::rollRatePIDComputation(double current, double desired){
 
 
 double Control::PitchPIDComputation(double current, double desired){
-    double Kp = 0.15;
-    double Ki = 0.080;
+    double Kp = 0.12;
+    double Ki = 0.10;
     double Kd = 0.00200;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-pitchTime);
@@ -264,8 +264,8 @@ double Control::PitchPIDComputation(double current, double desired){
 }
 
 double Control::RollPIDComputation(double current, double desired){
-    double Kp = 0.15;
-    double Ki = 0.080;
+    double Kp = 0.12;
+    double Ki = 0.10;
     double Kd = 0.00200;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>> (now-rollTime);
