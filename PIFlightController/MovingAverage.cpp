764c716c4e0ddcb528c4a4d80f3fb066e9866d06
mmcglynn45 <ndmmcglynn@gmail.com>
2016-02-12 01:37:23 -0800

    Build Update

--- PIFlightController/MovingAverage.cpp
+++ PIFlightController/MovingAverage.cpp
@@ -15,6 +15,15 @@ MovingAverage::MovingAverage(){
     double newArray[windowSize];
     array = newArray;
     array[0] = 0;
+    array[1] = 0;
+    array[2] = 0;
+    array[3] = 0;
+    array[4] = 0;
+    array[5] = 0;
+    array[6] = 0;
+    array[7] = 0;
+    array[8] = 0;
+    array[9] = 0;
     counter = 0;
 }
 
