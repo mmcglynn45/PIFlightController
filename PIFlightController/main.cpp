507f065aeebca1d29058e4a2d7bb3562bbcba57e
mmcglynn45 <ndmmcglynn@gmail.com>
2016-02-12 19:14:50 -0800

    Build Update

--- PIFlightController/main.cpp
+++ PIFlightController/main.cpp
@@ -68,6 +68,7 @@ int main(void)
     piIMU.mXComp = 0;
     piIMU.mYComp = 0;
     while(!piIMU.updateIMU()){}
+    printf("Addes\n");
     piIMU.pitchComp = -piIMU.pitch->getAverage();
     piIMU.rollComp = -piIMU.roll->getAverage();
     piIMU.mXComp = -piIMU.mX->getAverage();
