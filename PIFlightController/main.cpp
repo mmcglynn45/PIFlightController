7c0f3d50b48d5c5d727f14fbd783d565750d1327
mmcglynn45 <ndmmcglynn@gmail.com>
2016-02-13 16:03:21 -0800

    Build Update

--- PIFlightController/main.cpp
+++ PIFlightController/main.cpp
@@ -166,7 +166,7 @@ int main(void)
                 //cout << "Yaw = " << piIMU.yaw << endl;
                 //cout << "MX = " << piIMU.mX << endl;
                 //cout << "MY = " << piIMU.mY << endl;
-                printf("Sonar Reading: %f \n",firstSonar.distance);
+                printf("Sonar Reading: %f \n",firstSonar.distance.getAverage());
             }
             
             //cout<< count <<endl;
