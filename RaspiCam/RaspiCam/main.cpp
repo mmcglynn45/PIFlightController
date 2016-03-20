53343815ba434f452fe4fd319b3a7fcd7e9c88db
mmcglynn45 <ndmmcglynn@gmail.com>
2016-03-20 15:03:33 -0700

    Build Update

--- RaspiCam/RaspiCam/main.cpp
+++ RaspiCam/RaspiCam/main.cpp
@@ -40,7 +40,7 @@ int main ( int argc,char **argv ) {
     unsigned char *data=new unsigned char[  Camera.getImageTypeSize (     raspicam::RASPICAM_FORMAT_GRAY )];
     for ( int i=0; i<nCount; i++ ) {
         Camera.grab();
-        Camera.retrieve ( data,    raspicam::RASPICAM_FORMAT_GRAY );
+        Camera.retrieve ( data,    raspicam::RASPICAM_FORMAT_IGNORE );
         if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
         printf("Camera spot R at 50,50: %i",data[1]);
 
@@ -51,7 +51,7 @@ int main ( int argc,char **argv ) {
     //save
     std::ofstream outFile ( "raspicam_image.ppm",std::ios::binary );
     outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
-    outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
+    outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_GRAY) );
     cout<<"Image saved at raspicam_image.ppm"<<endl;
     //free resrources
     delete data;
