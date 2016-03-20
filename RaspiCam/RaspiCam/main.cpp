da9c4548987f002068eaa57dfa9d1a8be7e538f6
mmcglynn45 <ndmmcglynn@gmail.com>
2016-03-20 13:35:32 -0700

    Build Update

---
+++ RaspiCam/RaspiCam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RaspiCam
+//
+//  Created by Matthew McGlynn on 3/20/16.
+//  Copyright (c) 2016 Matthew McGlynn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
