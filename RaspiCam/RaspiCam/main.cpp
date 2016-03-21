f5e4aec8615f08815057d7eb13ee4736cf8c231d
mmcglynn45 <ndmmcglynn@gmail.com>
2016-03-20 21:17:59 -0700

    Build Update

--- RaspiCam/RaspiCam/main.cpp
+++ RaspiCam/RaspiCam/main.cpp
@@ -34,7 +34,7 @@ int main ( int argc,char **argv ) {
     time_t timer_begin,timer_end;
     int nCount=1;
     raspicam::RaspiCam Camera; //Cmaera object
-    Camera.setCaptureSize(1000, 1000);
+    Camera.setCaptureSize(640, 480);
     Camera.setFormat(raspicam::RASPICAM_FORMAT_RGB);
     //Open camera
     cout<<"Opening Camera..."<<endl;
@@ -52,7 +52,7 @@ int main ( int argc,char **argv ) {
     unsigned char *data=new unsigned char[  Camera.getImageTypeSize (     raspicam::RASPICAM_FORMAT_RGB )];
     for ( int i=0; i<nCount; i++ ) {
         Camera.grab();
-        Camera.retrieve (data,raspicam::RASPICAM_FORMAT_RGB);
+        Camera.retrieve (data);
         //if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
         //printf("Camera spot R at 50,50: %i",data[1]);
         double centerVert;
