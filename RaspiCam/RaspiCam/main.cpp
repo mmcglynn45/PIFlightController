f64c10c986e704958ac2407fb73e1145790375f4
mmcglynn45 <ndmmcglynn@gmail.com>
2016-03-21 00:06:51 -0700

    Build Update

--- RaspiCam/RaspiCam/main.cpp
+++ RaspiCam/RaspiCam/main.cpp
@@ -64,8 +64,8 @@ int main ( int argc,char **argv ) {
             for (int j = 0; j<Camera.getWidth()*3;j+=3 ) {
                 if ((data[i*Camera.getWidth()+j+2]) > 150){
                     data[i*Camera.getWidth()+j+2] = 255;
-                    data[i*Camera.getWidth()+j+1] = 0;
-                    data[i*Camera.getWidth()+j] = 0;
+                    data[i*Camera.getWidth()+j+1] = 255;
+                    data[i*Camera.getWidth()+j] = 255;
                 } else{
                     //data[i*Camera.getWidth()+j+2] = 0;
                     //data[i*Camera.getWidth()+j+1] = 255;
