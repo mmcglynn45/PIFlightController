c209f89b3b3698bb5733a97b14a72013e976ce16
mmcglynn45 <ndmmcglynn@gmail.com>
2016-03-29 22:03:42 -0700

    Build Update

--- RaspiCam/RaspiCam/main.cpp
+++ RaspiCam/RaspiCam/main.cpp
@@ -144,6 +144,7 @@ int main ( int argc,char **argv ) {
         */ 
         
     }
+    newImage.saveImageToFile("testImageClass.ppm");
     auto t2 = std::chrono::high_resolution_clock::now();
     double secondsElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()/1000.0;
     cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  nCount/secondsElapsed  <<endl;
