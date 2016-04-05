5f8ddf77560a3fabf406646fb9702f70e7c27c17
mmcglynn45 <ndmmcglynn@gmail.com>
2016-04-04 21:44:14 -0700

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -137,8 +137,8 @@ void Control::ManageOrientation(double roll, double pitch, double yaw, double al
     double desiredPitch = mXPIDComputation(mX, 0);
     double desiredRoll = mYPIDComputation(mY, 0);
     
-    desiredPitch = inputNormalizer(-desiredPitch, -.5, .5);
-    desiredRoll = inputNormalizer(-desiredRoll, -.5, .5);
+    desiredPitch = inputNormalizer(-desiredPitch, -0, 0);
+    desiredRoll = inputNormalizer(-desiredRoll, -0, 0);
     double desiredPitchRate = PitchPIDComputation(pitch, 0);
     double pitchControl = pitchRatePIDComputation(pitchRate, desiredPitchRate);
     //std::cout<<"Pitch Control: "<<pitchControl<<std::endl;
