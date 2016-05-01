f83db2a7017b9b001d4eb0ebe9833062e31f6c04
mmcglynn45 <ndmmcglynn@gmail.com>
2016-05-01 14:37:41 -0700

    Build Update

--- PIFlightController/IMU.cpp
+++ PIFlightController/IMU.cpp
@@ -25,7 +25,7 @@ void IMU::setup(){
     imu->setGyroEnable(true);
     imu->setAccelEnable(true);
     imu->setCompassEnable(true);
-    roll.setup(50);
+    roll.setup(100);
     pitch.setup(25);
     yaw.setup(10);
     mX.setup(70);
