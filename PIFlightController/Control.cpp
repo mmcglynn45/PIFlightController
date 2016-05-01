af4481d7a3df322ad1560f9c6ca1bcef1b223f9f
mmcglynn45 <ndmmcglynn@gmail.com>
2016-05-01 14:48:19 -0700

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -281,7 +281,7 @@ double Control::PitchPIDComputation(double current, double desired){
 }
 
 double Control::RollPIDComputation(double current, double desired){
-    double Kp = 0.025;
+    double Kp = 0.035;
     double Ki = 0.00;
     double Kd = 0.07;
     std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
