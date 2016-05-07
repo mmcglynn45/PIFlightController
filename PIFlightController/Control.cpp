3eaf562b1c52610a0dc519d1ce4f428c3e559e61
mmcglynn45 <ndmmcglynn@gmail.com>
2016-05-07 09:44:17 -0700

    Build Update

--- PIFlightController/Control.cpp
+++ PIFlightController/Control.cpp
@@ -85,7 +85,7 @@ void Control::setup(){
 
 
 int Control::safetyCheck(double roll,double pitch){
-    if ((fabs(roll)>92) || (fabs(pitch)>20)) {
+    if ((fabs(roll)>12) || (fabs(pitch)>12)) {
         adjustYPMotor(0);
         adjustXPMotor(0);
         adjustYNMotor(0);
