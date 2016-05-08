065638fce30b9258fd982f4b7142df0b1dc8fef1
mmcglynn45 <ndmmcglynn@gmail.com>
2016-05-07 17:20:45 -0700

    Build Update

--- SonarReceiver/SonarReceiver/server.cpp
+++ SonarReceiver/SonarReceiver/server.cpp
@@ -21,15 +21,11 @@ int main(int argc, char *argv[])
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
+    portno = 55555;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
